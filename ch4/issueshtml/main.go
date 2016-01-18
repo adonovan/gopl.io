c8852cb2dafe1e542940d20ce028c4c61d8d5074
Alan Donovan <alan@alandonovan.net>
2016-01-17 21:17:32 -0500

    Update to gobook@039ca5f7ddfb44bec546dbdc940c015d6e3912e6

--- ch4/issueshtml/main.go
+++ ch4/issueshtml/main.go
@@ -27,7 +27,7 @@ var issueList = template.Must(template.New("issuelist").Parse(`
 </tr>
 {{range .Items}}
 <tr>
-  <td><a href='{{.HTMLURL}}'>{{.Number}}</td>
+  <td><a href='{{.HTMLURL}}'>{{.Number}}</a></td>
   <td>{{.State}}</td>
   <td><a href='{{.User.HTMLURL}}'>{{.User.Login}}</a></td>
   <td><a href='{{.HTMLURL}}'>{{.Title}}</a></td>
