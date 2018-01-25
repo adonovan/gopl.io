0c7cfe42153abc48ca10b0565207ce6b73dce1cf
Alex Rice <rice.guido@gmail.com>
2018-01-24 22:42:54 -0700

    ex1.1

---
+++ ch1/ex1_1.go
@@ -0,0 +1,19 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 8.
+// Echo3 prints its command-line arguments.
+// ex 1.1 solution
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	fmt.Println(os.Args[0])
+	fmt.Println(strings.Join(os.Args[1:], " "))
+}
