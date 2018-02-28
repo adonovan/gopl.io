5a5fb25b7e05c220d054c270975b8385819f8714
Alex Rice <rice.guido@gmail.com>
2018-02-27 21:48:56 -0700

    implement tests and benchmarks for ex1.3

---
+++ ch1/ex1_3/ex1_3_test.go
@@ -0,0 +1,38 @@
+package ex1_3
+
+import (
+	"testing"
+)
+
+func TestEcho1(t *testing.T) {
+	input := []string{"cmd", "a", "b", "c" }
+	want := "a b c"
+	got := echo1(input)
+	if got != want {
+		t.Errorf("echo1(%v) = %v, want %v", input, got, want)
+	}
+}
+
+func BenchmarkEcho1(b *testing.B) {
+	input := []string{"cmd", "a", "b", "c" }
+	for i := 0; i < b.N; i++ {
+		echo1(input)
+	}
+}
+
+func TestEcho3(t *testing.T) {
+	input := []string{"cmd", "a", "b", "c" }
+	want := "a b c"
+	got := echo3(input)
+	if got != want {
+		t.Errorf("echo1(%v) = %v, want %v", input, got, want)
+	}
+}
+
+func BenchmarkEcho3(b *testing.B) {
+	input := []string{"cmd", "a", "b", "c" }
+	for i := 0; i < b.N; i++ {
+		echo3(input)
+	}
+}
+
