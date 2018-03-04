40df8ee2f2e0f09dcbda9ae4e5e42bbfc6d2e593
Alex Rice <rice.guido@gmail.com>
2018-03-04 07:33:46 -0700

    ex 1.9

---
+++ ch1/ex1_9/main.go
@@ -0,0 +1,38 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 16.
+// Fetch prints the content found at each specified URL.
+
+// ex 1.9 modify fetch to also print the http status code, found in resp.Status
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		if ! strings.HasPrefix(url, "http") {
+			url = "http://" + url
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Fprintf(os.Stdout, "[url: %s status: %d]\n\n", url,
+			resp.StatusCode)
+		_, err = io.Copy(os.Stdout, resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+	}
+}
