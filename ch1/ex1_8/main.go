97e439bbf41cec95c32e0fa79d76ae1395b5b0c8
Alex Rice <rice.guido@gmail.com>
2018-03-04 07:28:36 -0700

    ex 1.8

---
+++ ch1/ex1_8/main.go
@@ -0,0 +1,37 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 16.
+// Fetch prints the content found at each specified URL.
+
+// ex 1.8 modify fetch to add the prefix http:// to each argument URL if it is
+// missing. You might want to use strings.HasPrefix.
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		if ! strings.HasPrefix(url, "http") {
+			url = "http://" + url
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		_, err = io.Copy(os.Stdout, resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+	}
+}
