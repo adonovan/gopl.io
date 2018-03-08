24aa4eaaddeb196f1849bc4c89a472729fe14ecc
Alex Rice <rice.guido@gmail.com>
2018-03-08 05:59:28 -0700

    comments

--- ch2/ex2_1/conv.go
+++ ch2/ex2_1/conv.go
@@ -15,10 +15,12 @@ func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
 // FToC converts a Fahrenheit temperature to Celsius.
 func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
 
+// KToC converts a Kelvin temperature to Celsius
 func KToC(k Kelvin) Celsius {
 	return Celsius(k) + Celsius(float64(AbsoluteZeroC))
 }
 
+// CToK converts a Celsius temperature to Kelvin
 func CToK(c Celsius) Kelvin {
 	return Kelvin(c - AbsoluteZeroC)
 }
