dc9e74e0b3d92c9690b7a39fbc60a84d28e55b3b
Alex Rice <rice.guido@gmail.com>
2018-03-18 13:10:51 -0600

    ex 3.2

---
+++ ch3/ex3_2/doc.go
@@ -0,0 +1,9 @@
+/*
+ * Copyright © 2018 Alex G Rice
+ * License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+ */
+
+// ex3.2 experiment with visualizations of other functions from the math package.
+// can you produce an egg box, moguls, or a saddle?
+
+package ex3_2
