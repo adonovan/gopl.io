d8aa6f10787854be6de46f36c5ff073d8cd2765c
Alex Rice <rice.guido@gmail.com>
2018-03-17 20:50:14 -0600

    ex2.2

--- ch2/ex2_2/conv/cmd/main.go
+++ ch2/ex2_2/conv/cmd/main.go
@@ -14,14 +14,47 @@ import (
 	"github.com/guidorice/gopl.io/ch2/ex2_2/conv"
 )
 
+const usage = `
+converts weights, distances and temperatures. supported conversions: 
+
+* weight *
+pounds    -> kilograms
+stone     -> kilograms
+kilograms -> pounds
+kilograms -> stone
+
+* distance *
+feet   -> meters
+yards  -> meters
+meters -> feet
+meters -> yards
+
+* temperature *
+celsius -> fahrenheit
+fahrenheit -> celsius
+kelvin -> celsius
+celsius -> kelvin
+
+example usage (kelvin to celsius):
+
+	conv -k 1000 -out c
+	726.85°C
+
+`
+
 func main() {
 	conv.TempFlags()
 	conv.DistFlags()
 	conv.WeightFlags()
-	toScale := flag.String("out", "", "output scale: c, f, or k")
+	toScale := flag.String(
+		"out",
+		"",
+		"output scale: c, f, k, ft, m, y, kg, lb, st",
+	)
 	flag.Parse()
 	if *toScale == "" {
 		flag.Usage()
+		fmt.Print(usage)
 		os.Exit(1)
 	}
 	// try temperature conversions
