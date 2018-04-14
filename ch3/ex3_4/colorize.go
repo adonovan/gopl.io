239577c42a601de9a0f96034291dfb23bf285000
Alex G Rice <rice.guido@gmail.com>
2018-04-14 17:57:56 -0600

    ex 3.4 - wip

---
+++ ch3/ex3_4/colorize.go
@@ -0,0 +1,75 @@
+/*
+ * Copyright © 2018 Alex G Rice
+ * License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+ *
+ * Copyright (c) 2013 Lucas Beyer
+ * License: MIT
+ * https://github.com/lucasb-eyer/go-colorful/tree/master/doc/gradientgen
+ */
+
+package ex3_4
+
+import (
+	"log"
+
+	"github.com/lucasb-eyer/go-colorful"
+)
+
+// The "keypoints" of the gradient.
+var gradient = GradientTable{
+	{MustParseHex("#9e0142"), 0.0},
+	{MustParseHex("#d53e4f"), 0.1},
+	{MustParseHex("#f46d43"), 0.2},
+	{MustParseHex("#fdae61"), 0.3},
+	{MustParseHex("#fee090"), 0.4},
+	{MustParseHex("#ffffbf"), 0.5},
+	{MustParseHex("#e6f598"), 0.6},
+	{MustParseHex("#abdda4"), 0.7},
+	{MustParseHex("#66c2a5"), 0.8},
+	{MustParseHex("#3288bd"), 0.9},
+	{MustParseHex("#5e4fa2"), 1.0},
+}
+
+// This table contains the "keypoints" of the colorgradient you want to generate.
+// The position of each keypoint has to live in the range [0,1]
+type GradientTable []struct {
+	Col colorful.Color
+	Pos float64
+}
+
+// This is the meat of the gradient computation. It returns a HCL-blend between
+// the two colors around `t`.
+// Note: It relies heavily on the fact that the gradient keypoints are sorted.
+func (self GradientTable) GetInterpolatedColorFor(t float64) colorful.Color {
+	for i := 0; i < len(self)-1; i++ {
+		c1 := self[i]
+		c2 := self[i+1]
+		if c1.Pos <= t && t <= c2.Pos {
+			// We are in between c1 and c2. Go blend them!
+			t := (t - c1.Pos) / (c2.Pos - c1.Pos)
+			return c1.Col.BlendHcl(c2.Col, t).Clamped()
+		}
+	}
+
+	// Nothing found? Means we're at (or past) the last gradient keypoint.
+	return self[len(self)-1].Col
+}
+
+// This is a very nice thing Golang forces you to do!
+// It is necessary so that we can write out the literal of the colortable below.
+func MustParseHex(s string) colorful.Color {
+	c, err := colorful.Hex(s)
+	if err != nil {
+		panic("MustParseHex: " + err.Error())
+	}
+	return c
+}
+
+func Colorize(value, valley, peak float64) colorful.Color {
+	// get normalized height in range of 0,1
+	z, err := Normalize(valley, peak, value)
+	if err != nil {
+		log.Fatalf("error: Colorize: %v", err)
+	}
+	return gradient.GetInterpolatedColorFor(z)
+}
