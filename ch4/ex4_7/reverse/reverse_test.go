cd6e7208cba1fa5c2611b0caa8d4ce952d36ada5
Alex G Rice <rice.guido@gmail.com>
2018-07-20 07:20:07 -0600

    ex 4.7

---
+++ ch4/ex4_7/reverse/reverse_test.go
@@ -0,0 +1,33 @@
+package reverse
+
+import (
+	"testing"
+)
+
+func cases() map[int][]string {
+	var cases = map[int][]string{}
+	cases[0] = []string{
+		"tap",
+		"pat",
+	}
+	cases[1] = []string{
+		"hero",
+		"oreh",
+	}
+	cases[2] = []string{
+		"tacocat",
+		"tacocat",
+	}
+	return cases
+}
+
+func TestReverse(t *testing.T) {
+	for _, c := range cases() {
+		test := []byte(c[0])
+		want := []byte(c[1])
+		Reverse(test)
+		if string(test) != string(want) {
+			t.Errorf("Reverse: got %v, want %v", string(test), string(want))
+		}
+	}
+}
