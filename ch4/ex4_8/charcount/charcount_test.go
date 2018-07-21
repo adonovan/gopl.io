f81b0df50499784ac785e3c91933e70b13c62537
Alex G Rice <rice.guido@gmail.com>
2018-07-21 08:11:46 -0600

    convert test to use struct literal, subtests and table-driven test.

--- ch4/ex4_8/charcount/charcount_test.go
+++ ch4/ex4_8/charcount/charcount_test.go
@@ -9,47 +9,63 @@ import (
 	"testing"
 )
 
-var cases = map[string]Counts{}
+type CharcountTest struct {
+	Input string
+	Want  Counts
+}
 
-func init() {
-	cases["hi"] = Counts{
-		characters: 2,
-		graphic:    2,
-		letter:     2,
-		lowerCase:  2,
-	}
-	cases["hi5"] = Counts{
-		characters: 3,
-		digit:      1,
-		number:     1,
-		graphic:    3,
-		letter:     2,
-		lowerCase:  2,
-	}
-	cases["Fnord!"] = Counts{
-		characters:  6,
-		graphic:     6,
-		letter:      5,
-		lowerCase:   4,
-		upperCase:   1,
-		punctuation: 1,
-	}
-	cases["\b"] = Counts{
-		characters: 1,
-		control:    1,
-	}
-	cases["℃"] = Counts{
-		characters: 1,
-		graphic:    1,
-		symbol:     1,
-	}
+var tests = []CharcountTest{
+	{
+		"hi", Counts{
+			characters: 2,
+			graphic:    2,
+			letter:     2,
+			lowerCase:  2,
+		},
+	}, {
+		"hi5", Counts{
+			characters: 3,
+			digit:      1,
+			number:     1,
+			graphic:    3,
+			letter:     2,
+			lowerCase:  2,
+		},
+	}, {
+		"Fnord!", Counts{
+			characters:  6,
+			graphic:     6,
+			letter:      5,
+			lowerCase:   4,
+			upperCase:   1,
+			punctuation: 1,
+		},
+	},
+	{
+		"\b", Counts{
+			characters: 1,
+			control:    1,
+		},
+	}, {
+		"℃", Counts{
+			characters: 1,
+			graphic:    1,
+			symbol:     1,
+		},
+	},
 }
 
 func TestCharcount(t *testing.T) {
-	for test, want := range cases {
-		got := Charcount(test)
-		if got != want {
-			t.Errorf("Charcount: for %s\ngot:\n%+v\nwant:\n%+v", test, got, want)
-		}
+	for _, tc := range tests {
+		t.Run(tc.Input, func(t *testing.T) {
+			got := Charcount(tc.Input)
+			if got != tc.Want {
+				t.Errorf("Charcount: for %s\ngot:\n%+v\nwant:\n%+v",
+					tc.Input,
+					got,
+					tc.Want,
+				)
+			}
+		})
 	}
 }
