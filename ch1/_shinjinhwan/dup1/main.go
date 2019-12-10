04a03c38607ab036d4c3310d6a5c940943287cf5
Jin <jin.s@onther.io>
2019-12-11 02:20:23 +0900

    dup? update & example 1-4 solution

---
+++ ch1/_shinjinhwan/dup1/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+
+	for line, n := range counts {
+		if n > 1 {
+			// %d : digit
+			// %s : string
+			fmt.Println("%d\t%s\n", n, line)
+		}
+	}
+}
--- ch1/_shinjinhwan/ex1-4/main.go
+++ ch1/_shinjinhwan/ex1-4/main.go
@@ -8,10 +8,11 @@ import (
 
 func main() {
 	counts := make(map[string]int)
+	dupfiles := make(map[string][]string)
 	files := os.Args[1:]
 
 	if len(files) == 0 {
-		countLines(os.Stdin, counts)
+		countLines(os.Stdin, counts, dupfiles)
 	} else {
 		for _, arg := range files {
 			f, err := os.Open(arg)
@@ -20,19 +21,22 @@ func main() {
 				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
 				continue
 			}
-			countLines(f, counts)
+			countLines(f, counts, dupfiles)
 		}
 	}
 	for line, n := range counts {
 		if n > 1 {
-			fmt.Println("%d\t%s\n", n, line)
+			fmt.Printf("%d\t%s\n", n, line)
+			fmt.Printf("%s\t%v\n", line, dupfiles[line])
 		}
 	}
+
 }
 
-func countLines(f *os.File, counts map[string]int) {
+func countLines(f *os.File, counts map[string]int, dupfiles map[string][]string) {
 	input := bufio.NewScanner(f)
 	for input.Scan() {
 		counts[input.Text()]++
+		dupfiles[input.Text()] = append(dupfiles[input.Text()], f.Name())
 	}
 }
