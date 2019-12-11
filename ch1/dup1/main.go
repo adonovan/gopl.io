aa1a24370e7825c495b7f2eb5d8e271daaf05de8
rjwehrle <rjwehrle@gmail.com>
2019-12-10 22:24:52 -0500

    Duplicate text and count does not print
    Hello - I purchased a digital copy of "The Go Programming Language" and this example does not appear to print the text and count to the console. For reference, I am using version 1.12.7 of the go language. If the logic that iterates over the map of counts is moved inside the standard input scanner then I receive the appropriate log statements.

--- ch1/dup1/main.go
+++ ch1/dup1/main.go
@@ -19,11 +19,12 @@ func main() {
 	input := bufio.NewScanner(os.Stdin)
 	for input.Scan() {
 		counts[input.Text()]++
-	}
-	// NOTE: ignoring potential errors from input.Err()
-	for line, n := range counts {
-		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line)
+		
+		// NOTE: ignoring potential errors from input.Err()
+		for line, n := range counts {
+			if n > 1 {
+				fmt.Printf("%d\t%s\n", n, line)
+			}
 		}
 	}
 }
