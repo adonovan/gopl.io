3fcb4d2a6a3cc5fbd0cd9ea3acbfd7e279f35ca1
s-flow <kim23rim@gmail.com>
2019-12-11 23:07:59 +0900

    add lissajous example

--- ch1/_kimhyerim/lissajous/main.go
+++ ch1/_kimhyerim/lissajous/main.go
@@ -6,3 +6,55 @@
 //!+main
 
 // Lissajous generates GIF animations of random Lissajous figures.
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"math"
+	"math/rand"
+	"os"
+)
+
+var palette = []color.Color{color.White, color.Black}
+
+const (
+	WhiteIndex = 0 // palette color index -0
+	BlackIndex = 1 // palette color index -1
+)
+
+func main() {
+	// exec command : go run ./main.go > out.gif
+	lissajous(os.Stdout)
+}
+
+func lissajous(out io.Writer) {
+	const (
+		cycles  = 5
+		res     = 0.001
+		size    = 100
+		nframes = 64
+		delay   = 8
+	)
+
+	freq := rand.Float64() * 3.0
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0
+
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), BlackIndex)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+
+	gif.EncodeAll(out, &anim)
+}
