79fae68b17a140b771edf0902ba55ca13375f72a
Hyeongyu An <hyeongyu.an@actiopower.kr>
2019-12-14 18:55:06 +0900

    2019/12/14 commit

---
+++ ch1/_anhyeongyu/server3/main.go
@@ -0,0 +1,33 @@
+// 핸들러는 HTTP 요청을 반환한다.
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe("localhost:8001", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "%s %s %s\n", r.Method, r.URL, r.Proto)
+	for k, v := range r.Header {
+		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+	}
+	fmt.Fprintf(w, "Host = %q\n", r.Host)
+	fmt.Fprintf(w, "RemoteAddr = %q\n", r.RemoteAddr)
+	if err := r.ParseForm(); err != nil {
+		log.Print(err)
+	}
+	// 상동
+	// err := r.ParseForm()
+	// if err != nil {
+	// 	log.Print(err)
+	// }
+	for k, v := range r.Form {
+		fmt.Fprintf(w, "Form[%q] = %q\n", k, v)
+	}
+}
