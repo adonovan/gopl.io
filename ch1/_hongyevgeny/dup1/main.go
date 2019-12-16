45666c7828ef64492a5cee1a25f543f53f68589a
yevgeny <hongyevgeny@gmail.com>
2019-12-16 23:48:08 +0900

    fmt all code

--- ch1/_hongyevgeny/dup1/main.go
+++ ch1/_hongyevgeny/dup1/main.go
@@ -6,7 +6,7 @@ import (
 	"os"
 )
 
-func main(){
+func main() {
 	counts := make(map[string]int)
 	input := bufio.NewScanner(os.Stdin)
 	for input.Scan() {
@@ -15,7 +15,7 @@ func main(){
 
 	for line, n := range counts {
 		if n > 1 {
-			fmt.Printf("%d\t%s\n",n,line);
+			fmt.Printf("%d\t%s\n", n, line)
 		}
 	}
 }
--- ch1/_hongyevgeny/echo1/main.go
+++ ch1/_hongyevgeny/echo1/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	var s, sep string
-	for i :=1;i < len(os.Args); i++ {
+	for i := 1; i < len(os.Args); i++ {
 		s += sep + os.Args[i]
 		sep = " "
 	}
