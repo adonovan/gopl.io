3c874f122a24c523eb77ce240f6553ced3c56d9b
BlackwoolfMacDev <mud0107@chol.com>
2019-12-24 16:05:17 +0900

    2장 나머지 전체 cf popcount tempconv tempconv0 추가.....!!

--- ch2/tempconv/conv.go
+++ ch2/tempconv/conv.go
@@ -1,16 +1,12 @@
-// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-
-// See page 41.
-
-//!+
-
 package tempconv
 
-// CToF converts a Celsius temperature to Fahrenheit.
-func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
+// CToF 는 섭씨온도를 화씨온도로 변환한다.
+func CToF(c Celsius) Fahrenheit {
+	return Fahrenheit(c*9/5 + 32)
+}
 
-// FToC converts a Fahrenheit temperature to Celsius.
-func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
+// FToC 는 화씨온도를 썹시온도로 변환한다.
+func FToC(f Fahrenheit) Celsius {
+	return Celsius((f-32) * 5 / 9)
+}
 
-//!-
--- ch2/tempconv/tempconv.go
+++ ch2/tempconv/tempconv.go
@@ -1,9 +1,3 @@
-// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-
-//!+
-
-// Package tempconv performs Celsius and Fahrenheit conversions.
 package tempconv
 
 import "fmt"
@@ -12,12 +6,15 @@ type Celsius float64
 type Fahrenheit float64
 
 const (
-	AbsoluteZeroC Celsius = -273.15
-	FreezingC     Celsius = 0
-	BoilingC      Celsius = 100
+	AbsoluteZeroC	Celsius = -273.15
+	FreezingC		Celsius = 0
+	BoilingC		Celsius = 100
 )
 
-func (c Celsius) String() string    { return fmt.Sprintf("%g°C", c) }
-func (f Fahrenheit) String() string { return fmt.Sprintf("%g°F", f) }
+func (c Celsius) String() string {
+	return fmt.Sprintf("%g°C", c)
+}
 
-//!-
+func (f Fahrenheit) String () string  {
+	return fmt.Sprintf("%g°F", f)
+}
