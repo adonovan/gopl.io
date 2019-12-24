3af9c8be1ed0923c6326b2fa42bd05527d3560f0
BlackwoolfMacDev <mud0107@chol.com>
2019-12-24 21:03:33 +0900

    2장 전체 단 예제 ex2-4, ex2-5 빠짐

---
+++ ch2/_kimhyegon/ex2-2/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"gopl.io/ch2/tempconv"
+	"os"
+	"strconv"
+)
+
+func main() {
+	var inputArg []string
+	if len(os.Args) <= 1 {
+
+		input := bufio.NewScanner(os.Stdin)
+		for input.Scan() {
+			inputArg = append(inputArg, input.Text())
+		}
+
+		fmt.Println(inputArg)
+
+	} else {
+		for _, arg := range os.Args[1:] {
+			inputArg = append(inputArg, arg)
+
+		}
+	}
+
+	for _, arg := range inputArg {
+		t, err := strconv.ParseFloat(arg, 64)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "cf: %v\n", err)
+			os.Exit(1)
+		}
+
+		f := tempconv.Fahrenheit(t)
+		c := tempconv.Celsius(t)
+
+		fmt.Printf("%s = %s, %s = %s\n",
+			f, tempconv.FToC(f), c, tempconv.CToF(c))
+	}
+}
