06136fd0d1c0261882aa87c797c2b0117d32aefa
Blackwoolf <mud0107@chol.com>
2020-01-13 00:36:02 +0900

    5장

---
+++ ch5/_kimhyegon/trace/main.go
@@ -0,0 +1,26 @@
+// The trace program uses defer to add entry/exit diagnostics to a function.
+package main
+
+import (
+	"log"
+	"time"
+)
+
+//!+main
+func bigSlowOperation() {
+	defer trace("bigSlowOperation")() // don't forget the extra parentheses
+	// ...lots of work...
+	time.Sleep(10 * time.Second) // simulate slow operation by sleeping
+}
+
+func trace(msg string) func() {
+	start := time.Now()
+	log.Printf("enter %s", msg)
+	return func() { log.Printf("exit %s (%s)", msg, time.Since(start)) }
+}
+
+//!-main
+
+func main() {
+	bigSlowOperation()
+}
