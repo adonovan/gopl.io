4b55cf29f72ef6dce1a1c749d1733b29f0c90180
Hyunseok Jeong <hyunseok.jeong@irisid.com>
2020-02-10 06:38:08 -0500

    jhs ch12. till methods

--- ch12/_jeonghyunseok/presentation/methods/methods.go
+++ ch12/_jeonghyunseok/presentation/methods/methods.go
@@ -1,9 +1,6 @@
-// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+// 메쏘드는 뭐하는 녀석일까? 
+// 어떠한 value 이든 그 method 를 출력해준다.
 
-// See page 351.
-
-// Package methods provides a function to print the methods of any value.
 package methods
 
 import (
@@ -12,18 +9,15 @@ import (
 	"strings"
 )
 
-//!+print
-// Print prints the method set of the value x.
+
 func Print(x interface{}) {
 	v := reflect.ValueOf(x)
 	t := v.Type()
 	fmt.Printf("type %s\n", t)
 
-	for i := 0; i < v.NumMethod(); i++ {
+	for i:=0; i<v.NumMethod(); i++ {
 		methType := v.Method(i).Type()
-		fmt.Printf("func (%s) %s%s\n", t, t.Method(i).Name,
+		fmt.Printf("func(%s) %s%s\n", t, t.Method(i).Name, 
 			strings.TrimPrefix(methType.String(), "func"))
 	}
 }
-
-//!-print
--- ch12/_jeonghyunseok/presentation/methods/methods_test.go
+++ ch12/_jeonghyunseok/presentation/methods/methods_test.go
@@ -1,5 +1,4 @@
-// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+// 실제 테스트를 보자
 
 package methods_test
 
@@ -7,43 +6,64 @@ import (
 	"strings"
 	"time"
 
-	"gopl.io/ch12/methods"
+	"github.com/gopl-study/gopl.io/ch12/_jeonghyunseok/presentation/methods"
 )
 
 func ExamplePrintDuration() {
 	methods.Print(time.Hour)
 	// Output:
 	// type time.Duration
-	// func (time.Duration) Hours() float64
-	// func (time.Duration) Minutes() float64
-	// func (time.Duration) Nanoseconds() int64
-	// func (time.Duration) Seconds() float64
-	// func (time.Duration) String() string
+	// func(time.Duration) Hours() float64
+	// func(time.Duration) Microseconds() int64
+	// func(time.Duration) Milliseconds() int64
+	// func(time.Duration) Minutes() float64
+	// func(time.Duration) Nanoseconds() int64
+	// func(time.Duration) Round(time.Duration) time.Duration
+	// func(time.Duration) Seconds() float64
+	// func(time.Duration) String() string
+	// func(time.Duration) Truncate(time.Duration) time.Duration
 }
+
+/*
+아래와 같이 지원 메쏘드가 늘어났다.
+got:
+type time.Duration
+func(time.Duration) Hours() float64
+func(time.Duration) Microseconds() int64
+func(time.Duration) Milliseconds() int64
+func(time.Duration) Minutes() float64
+func(time.Duration) Nanoseconds() int64
+func(time.Duration) Round(time.Duration) time.Duration
+func(time.Duration) Seconds() float64
+func(time.Duration) String() string
+func(time.Duration) Truncate(time.Duration) time.Duration
+want:
+type time.Duration
+func (time.Duration) Hours() float64
+func (time.Duration) Minutes() float64
+func (time.Duration) Nanoseconds() int64
+func (time.Duration) Seconds() float64
+func (time.Duration) String() string
+--- FAIL: ExamplePrintReplacer (0.00s)
+*/
 
 func ExamplePrintReplacer() {
 	methods.Print(new(strings.Replacer))
 	// Output:
 	// type *strings.Replacer
-	// func (*strings.Replacer) Replace(string) string
-	// func (*strings.Replacer) WriteString(io.Writer, string) (int, error)
+	// func(*strings.Replacer) Replace(string) string
+	// func(*strings.Replacer) WriteString(io.Writer, string) (int, error)
 }
 
 /*
-//!+output
-methods.Print(time.Hour)
-// Output:
-// type time.Duration
-// func (time.Duration) Hours() float64
-// func (time.Duration) Minutes() float64
-// func (time.Duration) Nanoseconds() int64
-// func (time.Duration) Seconds() float64
-// func (time.Duration) String() string
 
-methods.Print(new(strings.Replacer))
-// Output:
-// type *strings.Replacer
-// func (*strings.Replacer) Replace(string) string
-// func (*strings.Replacer) WriteString(io.Writer, string) (int, error)
-//!-output
+이건 이전하고 똑같으니 성공할줄 알았는데 띄어쓰기 틀려서 실패. 허허
+got
+type *strings.Replacer
+func(*strings.Replacer) Replace(string) string
+func(*strings.Replacer) WriteString(io.Writer, string) (int, error)
+want:
+type *strings.Replacer
+func (*strings.Replacer) Replace(string) string
+func (*strings.Replacer) WriteString(io.Writer, string) (int, error)
 */
