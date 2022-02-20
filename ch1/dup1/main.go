37450ab8d046c7a60fd0ce18b562234a0624664b
tribhuvan <tribhuvanreddyramidi@gmail.com>
2022-02-20 20:37:45 +0530

    added potential error validation

--- ch1/dup1/main.go
+++ ch1/dup1/main.go
@@ -17,15 +17,30 @@ import (
 func main() {
 	counts := make(map[string]int)
 	input := bufio.NewScanner(os.Stdin)
+	//fmt.Println(input)
+	//var value int = 0
+	//value := 0
 	for input.Scan() {
+		if input.Text() == "end" { break }
+		//fmt.Println("input", input.Text())
 		counts[input.Text()]++
+		//fmt.Println(counts)
 	}
-	// NOTE: ignoring potential errors from input.Err()
-	for line, n := range counts {
-		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line)
+	// NOTE:added potential error check
+	if input.Err() != nil{
+        fmt.Print("err")
+    }
+	fmt.Println(counts)
+	for k, c := range counts {
+		if c>1 {
+			fmt.Printf("%s\t%d\n",k,c)
 		}
 	}
+	// for line, n := range counts {
+	// 	if n > 1 {
+	// 		fmt.Printf("%d\t%s\n", n, line)
+	// 	}
+	// }
 }
 
 //!-
