78537d5b48806808a645a175a57ee222bff746e5
tribhuvan <tribhuvanreddyramidi@gmail.com>
2022-02-21 15:57:47 +0530

    http:// prefix check and status print

--- ch1/fetch/main.go
+++ ch1/fetch/main.go
@@ -12,16 +12,21 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
 	for _, url := range os.Args[1:] {
-
+		if(!strings.HasPrefix(url,"http://")){
+			url = "http://"+url
+		}
+		fmt.Println("url :: ", url)
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
 		}
+		fmt.Println("Status ::", resp.Status)
 		//b, err := ioutil.ReadAll(resp.Body)
 		_, err = io.Copy(os.Stdout, resp.Body)
 		defer resp.Body.Close()
