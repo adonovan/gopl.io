c1eabcec1dc3185fbdcc2ae666c0a97eb9318b24
tribhuvan <tribhuvanreddyramidi@gmail.com>
2022-02-22 18:10:49 +0530

    sample exercises

--- ch2/incrementpointer/main.go
+++ ch2/incrementpointer/main.go
@@ -4,14 +4,34 @@ import "fmt"
 
 func main()  {
 	v := 0
-	fmt.Println(incr(&v))
+	incr(&v)
+	fmt.Println(v)
+	incrWithNewVariable(&v)
+	fibonacci(6)
 }
 
 func incr(p *int) int  {
+	*p++
+	return *p
+}
+
+
+func incrWithNewVariable(p *int) int  {
 	w :=1
 	p = &w
-	fmt.Println("p ",p)
+	//fmt.Println("p ",p)
 	*p++
 	fmt.Println("*p ", *p)
 	return *p
+}
+
+
+//nth fibonacci number
+func fibonacci(n int) int {
+	x,y := 0,1
+	for i := 0; i < n; i++ {
+		x, y = y, x+y
+	}
+	fmt.Println("nth fibonacci number is ",x)
+	return x
 }
