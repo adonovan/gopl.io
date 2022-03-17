4cc877a4ad6d1f243c62cf3373b26d72a6a49d7b
gongqi.zhen <gongqi.zhen@gmail.com>
2022-03-17 16:26:39 +0900

    Fix Example Functions
    makoto@miyazakimakotonoMacBook-Pro methods % go doc time.Duration
    package time // import "time"
    
    type Duration int64
        A Duration represents the elapsed time between two instants as an int64
        nanosecond count. The representation limits the largest representable
        duration to approximately 290 years.
    
    func ParseDuration(s string) (Duration, error)
    func Since(t Time) Duration
    func Until(t Time) Duration
    func (d Duration) Hours() float64
    func (d Duration) Microseconds() int64
    func (d Duration) Milliseconds() int64
    func (d Duration) Minutes() float64
    func (d Duration) Nanoseconds() int64
    func (d Duration) Round(m Duration) Duration
    func (d Duration) Seconds() float64
    func (d Duration) String() string
    func (d Duration) Truncate(m Duration) Duration
    makoto@miyazakimakotonoMacBook-Pro methods % go test -v
    === RUN   ExamplePrintDuration
    --- PASS: ExamplePrintDuration (0.00s)
    === RUN   ExamplePrintReplacer
    --- PASS: ExamplePrintReplacer (0.00s)
    PASS
    ok  	gopl.io/ch12/methods	0.123s
    makoto@miyazakimakotonoMacBook-Pro methods %

--- ch12/methods/methods_test.go
+++ ch12/methods/methods_test.go
@@ -15,10 +15,14 @@ func ExamplePrintDuration() {
 	// Output:
 	// type time.Duration
 	// func (time.Duration) Hours() float64
+	// func (time.Duration) Microseconds() int64
+	// func (time.Duration) Milliseconds() int64
 	// func (time.Duration) Minutes() float64
 	// func (time.Duration) Nanoseconds() int64
+	// func (time.Duration) Round(time.Duration) time.Duration
 	// func (time.Duration) Seconds() float64
 	// func (time.Duration) String() string
+	// func (time.Duration) Truncate(time.Duration) time.Duration
 }
 
 func ExamplePrintReplacer() {
@@ -35,10 +39,14 @@ methods.Print(time.Hour)
 // Output:
 // type time.Duration
 // func (time.Duration) Hours() float64
+// func (time.Duration) Microseconds() int64
+// func (time.Duration) Milliseconds() int64
 // func (time.Duration) Minutes() float64
 // func (time.Duration) Nanoseconds() int64
+// func (time.Duration) Round(time.Duration) time.Duration
 // func (time.Duration) Seconds() float64
 // func (time.Duration) String() string
+// func (time.Duration) Truncate(time.Duration) time.Duration
 
 methods.Print(new(strings.Replacer))
 // Output:
