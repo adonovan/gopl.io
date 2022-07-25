5417a5e70305db395df4ad7568004d95618012c3
vsguts <vsguts@gmail.com>
2022-07-25 15:17:00 +0300

    Ch1: Ex 1.8 implemented.

--- ch1/fetch/main.go
+++ ch1/fetch/main.go
@@ -12,10 +12,15 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
 	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http") {
+			url = "http://" + url
+		}
+		fmt.Println(url)
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
@@ -28,6 +33,7 @@ func main() {
 		//	os.Exit(1)
 		//}
 		//fmt.Printf("%s", b)
+
 		_, err = io.Copy(os.Stdout, resp.Body)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
