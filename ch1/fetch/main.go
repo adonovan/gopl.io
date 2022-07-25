b160103436b5ad6d8eff346204cb11bd575db9ae
vsguts <vsguts@gmail.com>
2022-07-25 15:16:30 +0300

    Ch1: Ex 1.7 implemented.

--- ch1/fetch/main.go
+++ ch1/fetch/main.go
@@ -9,7 +9,7 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"os"
 )
@@ -21,13 +21,18 @@ func main() {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
 		}
-		b, err := ioutil.ReadAll(resp.Body)
-		resp.Body.Close()
+		//b, err := ioutil.ReadAll(resp.Body)
+		//resp.Body.Close()
+		//if err != nil {
+		//	fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+		//	os.Exit(1)
+		//}
+		//fmt.Printf("%s", b)
+		_, err = io.Copy(os.Stdout, resp.Body)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
 		}
-		fmt.Printf("%s", b)
 	}
 }
 
